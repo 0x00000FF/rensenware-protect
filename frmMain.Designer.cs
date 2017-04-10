8fa3028c663660753246152b40e0611ec93f4ca5
0x00000FF <doctor@kangnang.net>
2017-04-10 12:26:25 +0900

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace rensenware_protection
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Status = new System.Windows.Forms.Label();
+            this.Create = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "rensenWare Safety : ";
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.ForeColor = System.Drawing.Color.Red;
+            this.Status.Location = new System.Drawing.Point(142, 16);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(66, 12);
+            this.Status.TabIndex = 1;
+            this.Status.Text = "NOT SAFE";
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(15, 42);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(327, 46);
+            this.Create.TabIndex = 2;
+            this.Create.Text = "Create Pseudo-Key/IV";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(354, 100);
+            this.Controls.Add(this.Create);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Pseudo-IV/Key Generator for rensenWare";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Button Create;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rensenware_protection
+{
+    public partial class frmMain : Form
+    {
+        internal static readonly string KeyFilePath =
+            Environment.GetFolderPath(
+                Environment.SpecialFolder.Desktop
+                ) + @"\randomkey.bin";
+        internal static readonly string IVFilePath =
+            Environment.GetFolderPath(
+                Environment.SpecialFolder.Desktop
+                ) + @"\randomiv.bin";
+
+
+        public frmMain()
+        {
+            InitializeComponent();
+
+            Check();
+        }
+
+        private void Check()
+        {
+            if (File.Exists(IVFilePath))
+            {
+                var IVTest = File.ReadAllBytes(IVFilePath);
+                if (IVTest.Length == 16)
+                    if (File.Exists(KeyFilePath))
+                    {
+                        var KeyTest = File.ReadAllBytes(KeyFilePath);
+                        if (KeyTest.Length == 32)
+                        {
+                            Status.ForeColor = Color.LimeGreen;
+                            Status.Text = "SAFE for Original Build";
+                        }
+                    }
+            }
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            if(File.Exists(IVFilePath))
+            {
+                if(File.ReadAllBytes(IVFilePath).Length != 16)
+                {
+                    File.Delete(IVFilePath);
+                }
+
+                var _buffer = new byte[16];
+
+                var randomKey = new RNGCryptoServiceProvider();
+                randomKey.GetBytes(_buffer);
+                File.WriteAllBytes(IVFilePath, _buffer);
+            }
+            else
+            {
+                var _buffer = new byte[16];
+
+                var randomKey = new RNGCryptoServiceProvider();
+                randomKey.GetBytes(_buffer);
+                File.WriteAllBytes(IVFilePath, _buffer);
+            }
+
+            if (File.Exists(KeyFilePath))
+            {
+                if (File.ReadAllBytes(KeyFilePath).Length != 32)
+                {
+                    File.Delete(KeyFilePath);
+                }
+
+                var _buffer = new byte[32];
+
+                var randomKey = new RNGCryptoServiceProvider();
+                randomKey.GetBytes(_buffer);
+                File.WriteAllBytes(KeyFilePath, _buffer);
+            }
+            else
+            {
+                var _buffer = new byte[32];
+
+                var randomKey = new RNGCryptoServiceProvider();
+                randomKey.GetBytes(_buffer);
+                File.WriteAllBytes(KeyFilePath, _buffer);
+            }
+
+            File.SetAttributes(IVFilePath, FileAttributes.Hidden);
+            File.SetAttributes(KeyFilePath, FileAttributes.Hidden);
+
+            Check();
+        }
+    }
+}
